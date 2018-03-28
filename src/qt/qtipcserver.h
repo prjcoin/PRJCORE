bb2bca58a1bda84b1085dcd8f151819b7b425d5c
prjcoin <37875591+prjcoin@users.noreply.github.com>
2018-03-28 22:04:14 +0300

    Initial commit

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define PRJCoin-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "PRJCoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
